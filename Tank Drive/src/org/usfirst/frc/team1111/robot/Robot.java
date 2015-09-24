1a5af265d86e2fd4cbf3828b223c27b23df1fd88
Braidan <legohead259@gmail.com>
2015-09-24 19:03:45 -0400

    k

--- Tank Drive/src/org/usfirst/frc/team1111/robot/Robot.java
+++ Tank Drive/src/org/usfirst/frc/team1111/robot/Robot.java
@@ -18,7 +18,7 @@ import edu.wpi.first.wpilibj.Timer;
  * directory.
  *
  * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
- * don't. Unless you knw what you are doing, complex code will be much more difficult under
+ * don't. Unless you know what you are doing, complex code will be much more difficult under
  * this system. Use IterativeRobot or Command-Based instead if you're new.
  */
 public class Robot extends IterativeRobot
@@ -45,5 +45,5 @@ public class Robot extends IterativeRobot
         }
     }
 // THIS IS SPARTA!
-    //THIS IS SPARTA MK. 2!hgasfasdgjj
+    //THIS IS SPARTA MK. 2!
 }
